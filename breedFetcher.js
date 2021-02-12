c2b008d51d1fe90140ab8580120e575ca453997e
BettyHoPro <bettyvisualtech@gmail.com>
2021-02-12 04:32:59 +0000

    refactory the modules

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,22 @@
 const request = require("request");
 
-const searchBreed = (breedName) => {
+const fetchBreedDescription = (breedName, callback) => {
   request(
     `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
     (error, response, body) => {
       if (error) {
-        console.log(error);
+        callback(error);
         return;
       }
       const data = JSON.parse(body);
       if (data.length === 0) {
-        console.log("Sorry, we don't have this breed.");
+        callback("Sorry, we don't have this breed.");
         return;
       }
-      console.log(data[0].description);
+      callback(null, data[0].description);
     }
   );
 };
-searchBreed(process.argv[2]);
+//fetchBreedDescription(process.argv[2], callback);
+
+module.exports = { fetchBreedDescription };
